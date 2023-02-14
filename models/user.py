35e823027d359fe07c8093c79465e54ce64de427
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-02-14 21:58:51 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-"""
-Modelling the user of the application
-"""
-
-from base_model import BaseModel
+"""This module defines a class User"""
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """
-    User class for creating a user object
-    """
-    email: str = ""
-    password: str = ""
-    first_name: str = ""
-    last_name: str = ""
-
-    def __init__(self, *args, **kwargs):
-        """
-        Constructor for the user object
-        """
-        super().__init__(*args, **kwargs)
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
